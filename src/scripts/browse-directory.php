cc05f5a42f7c6d4c2b85b5f6225a3b05b8314b2f
Abhinav <in.abhi9@gmail.com>
2016-02-13 19:48:10 +0530

    Moved directing scanning to helper

--- src/scripts/browse-directory.php
+++ src/scripts/browse-directory.php
@@ -10,44 +10,13 @@ require_once __DIR__ . '/helper.php';
 session_start();
 // Base path
 $path = realpath($_SESSION['path']);
-$path = '/home/abhinav';
+$sortDirFirst = $_SESSION['sortDirFirst'];
+$showHiddenFile = $_SESSION['showHiddenFile'];
 // relative path to base path
 $relativePath = $_GET['path'];
+// absolute directory path
+$directory = $path . $relativePath;
 
 header('Content-Type: application/json');
 
-$response = [];
-$directory = $path . $relativePath;
-$scanned_directory = array_diff(scandir($directory), ['..', '.']);
-
-
-foreach ($scanned_directory as $item) {
-    $itemPath = $directory . '/' . $item;
-    $type = is_dir($itemPath) ? 'dir' : 'file';
-    $size = Helper::human_filesize($stats['size']);
-    $stats = stat($itemPath);
-
-
-    $data = [
-        'name'         => $item,
-        'type'         => $type,
-        'size'         => $type == 'dir' ? '--' : $size,
-        'lastModified' => date('F jS, Y \a\t', $stats['mtime']),
-        'relativePath' => "{$relativePath}/{$item}"
-    ];
-    $response[] = $data;
-}
-
-// Soring by directory first
-usort(
-    $response,
-    function ($a, $b) {
-        if ($a['type'] == $b['type']) {
-            return $a['name'] > $b['name'];
-        } else {
-            return $a['type'] != 'dir';
-        }
-    }
-);
-
-echo json_encode($response);
+echo json_encode(Helper::scanDir($directory, $relativePath, $showHiddenFile, $sortDirFirst));
--- src/scripts/helper.php
+++ src/scripts/helper.php
@@ -7,10 +7,60 @@
  * Time: 6:01 PM
  */
 class Helper {
+    static function scanDir(
+        $directory,
+        $relativePath,
+        $showHiddenFile = true,
+        $sortDirFirst = true
+    ) {
+        $response = [];
+        $scannedDirectory = array_diff(scandir($directory), ['..', '.']);
+
+        foreach ($scannedDirectory as $item) {
+            if ($showHiddenFile == false && strpos($item, '.') !== false) {
+                continue;
+            }
+
+            $itemPath = $directory . '/' . $item;
+            $type = is_dir($itemPath) ? 'dir' : 'file';
+            $stats = stat($itemPath);
+            $size = self::human_filesize($stats['size']);
+
+            $data = [
+                'name' => $item,
+                'type' => $type,
+                'size' => $type == 'dir' ? '--' : $size,
+                'lastModified' => date('F jS, Y \a\t', $stats['mtime']),
+                'relativePath' => "{$relativePath}/{$item}"
+            ];
+            $response[] = $data;
+        }
+
+        if ($sortDirFirst == true) {
+            self::sortDirFirst($response);
+        }
+
+        return $response;
+    }
+
     static function human_filesize($size, $precision = 2) {
         for ($i = 0; ($size / 1024) > 0.9; $i++, $size /= 1024) {
         }
 
         return round($size, $precision) . ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][$i];
     }
+
+    static function sortDirFirst(&$ar) {
+        usort(
+            $ar,
+            function ($a, $b) {
+                if ($a['type'] == $b['type']) {
+                    return $a['name'] > $b['name'];
+                } else {
+                    return $a['type'] != 'dir';
+                }
+            }
+        );
+    }
+
 }
