c153bbf3e92eaed94aa984c7b6e0dbf4a9b7779e
Abhinav <in.abhi9@gmail.com>
2016-02-13 22:19:57 +0530

    Add header

--- src/scripts/browser.php
+++ src/scripts/browser.php
@@ -10,6 +10,19 @@ include_once __DIR__ . '/../autoload.php';
         </h3>
     </div>
     <div class="panel-body" style="padding: 5px 16px 0px 16px">
+        <div class="row" style="margin-bottom: 5px">
+            <div class="col-md-7">
+                <strong>Name</strong>
+            </div>
+
+            <div class="col-md-3">
+                <strong>Last Modified</strong>
+            </div>
+
+            <div class="col-md-2">
+                <strong>Size</strong>
+            </div>
+        </div>
         <div id="ajaxBrowser"></div>
     </div>
 </div>
