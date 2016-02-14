fd029d0fe5f3f410ce3b3dcdd8fe47f872691d5f
Abhinav <in.abhi9@gmail.com>
2016-02-14 12:30:53 +0530

    Convert error to exception

--- src/autoload.php
+++ src/autoload.php
@@ -8,6 +8,14 @@
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
 
+set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
 spl_autoload_register(
     function ($class_name) {
         include_once __DIR__ . "/scripts/{$class_name}.php";
